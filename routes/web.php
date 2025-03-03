1d84db90fa472876439714fcdedb050c54a0bf94
ffauziee <142006451+ffauziee@users.noreply.github.com>
2025-03-03 10:08:20 +0700

    Modify

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LevelController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SalesController;
@@ -17,15 +18,21 @@ use App\Http\Controllers\SalesController;
 |
 */
 
-Route::get('/', [HomeController::class, 'index']);
-
-Route::prefix('category')->group(function () {
-    Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
-    Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
-    Route::get('/home-care', [ProductController::class, 'homeCare']);
-    Route::get('/baby-kid', [ProductController::class, 'babyKid']);
+Route::get('/',function(){
+    return view('welcome');
 });
 
-Route::get('/user/{id}/name/{name}', [UserController::class, 'show']);
+Route::get('/level', [LevelController::class, 'index']);
 
-Route::get('/sales', [SalesController::class, 'index']);
+// Route::get('/', [HomeController::class, 'index']);
+
+// Route::prefix('category')->group(function () {
+//     Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
+//     Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
+//     Route::get('/home-care', [ProductController::class, 'homeCare']);
+//     Route::get('/baby-kid', [ProductController::class, 'babyKid']);
+// });
+
+// Route::get('/user/{id}/name/{name}', [UserController::class, 'show']);
+
+// Route::get('/sales', [SalesController::class, 'index']);
