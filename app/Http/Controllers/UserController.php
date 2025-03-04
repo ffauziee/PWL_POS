4b5d1633359604e56250b1155710c2a9b800ee3f
ffauziee <142006451+ffauziee@users.noreply.github.com>
2025-03-04 23:37:53 +0700

    modify praktikum 2.7

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,34 +9,34 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index()
-    {
-        // $data =
-        //     [
-        //         'username' => 'manager_tiga',
-        //         'nama' => 'Manager 3',
-        //         'password' => Hash::make('12345'),
-        //         'level_id' => 2
-        //     ];
+    // public function index()
+    // {
+    //     // $data =
+    //     //     [
+    //     //         'username' => 'manager_tiga',
+    //     //         'nama' => 'Manager 3',
+    //     //         'password' => Hash::make('12345'),
+    //     //         'level_id' => 2
+    //     //     ];
 
-        // UserModel::create($data);
-        // $user = UserModel::all();
+    //     // UserModel::create($data);
+    //     // $user = UserModel::all();
 
-        // $user = UserModel::findOr(20, ['username', 'nama'], function(){
-        //     abort(404);
-        // });
+    //     // $user = UserModel::findOr(20, ['username', 'nama'], function(){
+    //     //     abort(404);
+    //     // });
 
-        // $user = UserModel::findOrFail(1);
+    //     // $user = UserModel::findOrFail(1);
 
-        // $user = UserModel::where('username', 'manager0')->firstOrFail()
+    //     // $user = UserModel::where('username', 'manager0')->firstOrFail()
 
 
-        // $user = UserModel::where('level_id', 2)->count();
-        // // dd($user);
+    //     // $user = UserModel::where('level_id', 2)->count();
+    //     // // dd($user);
 
-        $user = UserModel::all();
-        return view('user', ['data' => $user]);
-    }
+    //     $user = UserModel::all();
+    //     return view('user', ['data' => $user]);
+    // }
 
     public function tambah()
     {
@@ -81,5 +81,10 @@ class UserController extends Controller
         $user->delete();
 
         return redirect('/user');
+    }
+
+    public function index(){
+    $user = UserModel::with('level')->get();
+    dd($user);
     }
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -8,26 +8,30 @@
 </head>
 <body>
     <h1>Data User</h1>
-    <a href="user/tambah">Tambah User</a>
+    <a href="/user/tambah">+ Tambah User</a>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
             <td>ID</td>
             <td>Username</td>
             <td>Nama</td>
             <td>ID Level Pengguna</td>
-
+            <td>Kode Level</td>
+            <td>Nama Level</td>
+            <td>Aksi</td>
         </tr>
-        @foreach($data as $d)
+        @foreach ($data as $d)
         <tr>
-            <td>{{$d->user_id}}</td>
-            <td>{{$d->username}}</td>
-            <td>{{$d->nama}}</td>
-            <td>{{$d->level_id}}</td>
-            <td><a href="/user/ubah/{{$d->user_id}}">Ubah</a> | <a href="/user/hapus/{{$d->user_id}}">Hapus</a></td>
+            <td>{{ $d->user_id }}</td>
+            <td>{{ $d->username }}</td>
+            <td>{{ $d->nama }}</td>
+            <td>{{ $d->level_id }}</td>
+            <td>{{ $d->level->level_kode }}</td>
+            <td>{{ $d->level->level_nama }}</td>
+            <td><a href="/user/ubah/{{ $d->user_id }}">Ubah</a> | <a href="/user/hapus/{{ $d->user_id }}">Hapus</a></td>
         </tr>
         @endforeach
     </table>
-
+</body>
     {{-- <h1>Jumlah User dengan Level ID 2</h1>
     <table border="1">
         <tr>
@@ -37,5 +41,5 @@
             <td>{{ $data }}</td> <!-- Menampilkan jumlah user -->
         </tr>
     </table> --}}
-</body>
+{{-- </body> --}}
 </html>
